29dadb7d6fb8f83abf592d5c1533ee35a246bf7c
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-08-05 22:38:46 +0530

    Circular Tour (Non Queue approach)

---
+++ Stack_And_Queue/CircularTour.cpp
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+ // } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+        //Your code here
+        int deficit = 0, start = 0, cap = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            cap += p[i].petrol - p[i].distance;
+            if(cap < 0)
+            {
+                start = i+1;
+                deficit += cap;
+                cap = 0;
+            }
+        }
+        return cap + deficit >= 0 ? start : -1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+  // } Driver Code Ends
--- Stack_And_Queue/README.md
+++ Stack_And_Queue/README.md
@@ -54,6 +54,17 @@
         push(1)  the stack will be {2 1}
         getMin() min element will be 1
 
+## 7. Circular Tour (Direct Approach no Queue required):
+    Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
+        - The amount of petrol that every petrol pump has.
+        - Distance from that petrol pump to the next petrol pump.
+    Find a starting point where the truck can start to get through the complete circle without exhausting its petrol in between.
+    Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.
+    Example 1:
+    Input: N = 4 Petrol = 4 6 7 4 Distance = 6 5 3 5
+    Output: 1 
+    Explanation: There are 4 petrol pumps with amount of petrol and distance to next petrol pump value pairs as {4, 6}, {6, 5}, {7, 3} and {4, 5}. The first point from where truck can make a circular tour is 2nd petrol pump. Output in this case is 1 (index of 2nd petrol pump).
+
 ## 9. Rotten Oranges:
     Given a grid of dimension nxm where each cell in the grid can have values 0, 1 or 2 which has the following meaning:
     0 : Empty cell
