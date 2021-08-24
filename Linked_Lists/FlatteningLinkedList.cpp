cc60e3998d1d021c9e7b266c7969e86388e3fe98
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-08-24 08:20:13 +0530

    Flattening a Linked List

---
+++ Linked_Lists/FlatteningLinkedList.cpp
@@ -0,0 +1,144 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+Node *merge(Node *root1, Node *root2)
+{
+    if(!root1 && !root2)
+        return NULL;
+    if(!root1)
+        return root2;
+    if(!root2)
+        return root1;
+    Node *temp;
+    if(root1->data < root2->data)
+    {
+        temp = root1;
+        temp->bottom = merge(root1->bottom, root2);
+    }
+    else
+    {
+        temp = root2;
+        temp->bottom = merge(root1, root2->bottom);
+    }
+    temp->next = NULL;
+    return temp;
+}
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+Node *flatten(Node *root)
+{
+    // Your code here
+    if(!root || !root->next)
+        return root;
+    root->next = flatten(root->next);
+    root = merge(root, root->next);
+    return root;
+}
--- Linked_Lists/README.md
+++ Linked_Lists/README.md
@@ -24,3 +24,21 @@
         Rotate 1: 4 -> 7 -> 8 -> 9 -> 2
         Rotate 2: 7 -> 8 -> 9 -> 2 -> 4
         Rotate 3: 8 -> 9 -> 2 -> 4 -> 7
+
+## 9. Flattening a Linked List:
+    Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+    (i) a next pointer to the next node,
+    (ii) a bottom pointer to a linked list where this node is head.
+    Each of the sub-linked-list is in sorted order. Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+    Note: The flattened list will be printed using the bottom pointer instead of next pointer.
+    Example 1:
+    Input:
+        5 -> 10 -> 19 -> 28
+        |     |     |     | 
+        7     20    22   35
+        |           |     | 
+        8          50    40
+        |                 | 
+        30               45
+    Output:  5-> 7-> 8- > 10 -> 19-> 20->22-> 28-> 30-> 35-> 40-> 45-> 50.
+    Explanation: The resultant linked lists has every node in a single level. (Note: | represents the bottom pointer.)
