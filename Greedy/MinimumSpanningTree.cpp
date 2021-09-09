a8a938c87d9a3166b40b74661e73205b95a80d52
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-09-09 17:41:47 +0530

    Minimum Spanning Tree

---
+++ Greedy/MinimumSpanningTree.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        vector<bool> vis(V, false);
+        int res = 0;
+        while(!pq.empty())
+        {
+            auto top = pq.top();
+            pq.pop();
+            if(vis[top.second])
+                continue;
+            res += top.first;
+            vis[top.second] = 1;
+            int ind = top.second;
+            for(int i = 0 ; i < adj[ind].size() ; i++)
+            {
+                if(!vis[adj[ind][i][0]])
+                    pq.push({adj[ind][i][1], adj[ind][i][0]});
+            }
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
--- Greedy/README.md
+++ Greedy/README.md
@@ -86,6 +86,9 @@
     Input : abc
     Output : abc
 
+## 10. Minimum Spanning Tree:
+    Given a weighted, undirected and connected graph of V vertices and E edges. The task is to find the sum of weights of the edges of the Minimum Spanning Tree.
+
 ## 11. Shop in Candy Store:
     In a candy store, there are N different types of candies available and the prices of all the N different types of candies are provided to you. You are now provided with an attractive offer. You can buy a single candy from the store and get at most K other candies ( all are different types ) for free. Now you have to answer two questions. Firstly, you have to find what is the minimum amount of money you have to spend to buy all the N different candies. Secondly, you have to find what is the maximum amount of money you have to spend to buy all the N different candies. In both the cases you must utilize the offer i.e. you buy one candy and get K other candies for free.
     Example 1:
