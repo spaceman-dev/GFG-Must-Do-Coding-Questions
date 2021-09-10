e89aa97ca9f90b51558228a3ea40680085e3a72c
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-09-10 17:10:40 +0530

    Find first set bit

---
+++ Bit_Magic/FindFirstSetBit.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        // Your code here
+        return log2((n ^ (n - 1)) + 1);
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+  // } Driver Code Ends
---
+++ Bit_Magic/README.md
@@ -0,0 +1,8 @@
+# Bit Magic
+
+## 1. Find first set bit:
+    Given an integer an N. The task is to return the position of first set bit found from the right side in the binary representation of the number. Note: If there is no set bit in the integer N, then return 0 from the function.  
+    Example 1:
+    Input: N = 18
+    Output: 2
+    Explanation: Binary representation of 18 is 010010,the first set bit from the right side is at position 2.
