76d3a90a7f1071071bf0a8249e36ff67a43f44c0
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-09-16 23:37:30 +0530

    Bit Difference

---
+++ Bit_Magic/BitDifference.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b)
+    {
+        // Your logic here
+        int n = a ^ b;
+        int res = 0;
+        while(n)
+        {
+            n = (n & (n - 1));
+            res++;
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
--- Bit_Magic/README.md
+++ Bit_Magic/README.md
@@ -41,3 +41,11 @@
     Input: N = 1
     Output: true
     Explanation: 1 is equal to 2 raised to 0 (20 = 1).
+
+## 7. Bit Difference:
+    You are given two numbers A and B. The task is to count the number of bits needed to be flipped to convert A to B.
+    Example 1:
+    Input: A = 10, B = 20
+    Output: 4
+    Explanation: A  = 01010, B  = 10100
+    As we can see, the bits of A that need to be flipped are 01010. If we flip these bits, we get 10100, which is B.
