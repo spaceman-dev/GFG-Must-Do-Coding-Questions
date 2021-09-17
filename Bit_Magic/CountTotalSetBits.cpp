e9b15311c185076cc3822f574a6b5dc6d3e91464
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-09-17 11:07:14 +0530

    Count total set bits

---
+++ Bit_Magic/CountTotalSetBits.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int n)
+    {
+        // Your logic here
+        if(n == 0)
+            return 0;
+        int a = log2(n);
+        return ((pow(2, a - 1) * a) + (n - pow(2, a) + 1) + countSetBits(n - pow(2, a)));
+    }
+};
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+  // } Driver Code Ends
--- Bit_Magic/README.md
+++ Bit_Magic/README.md
@@ -66,4 +66,15 @@
     Input: N = 23
     Output: 43
     Explanation: Binary representation of the given number is 00010111 after swapping 00101011 = 43 in decimal.
-    
+
+## 10. Count total set bits:
+    You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+    Example 1:
+    Input: N = 4
+    Output: 5
+    Explanation: For numbers from 1 to 4.
+                    For 1: 0 0 1 = 1 set bits
+                    For 2: 0 1 0 = 1 set bits
+                    For 3: 0 1 1 = 2 set bits
+                    For 4: 1 0 0 = 1 set bits
+                Therefore, the total set bits is 5.
