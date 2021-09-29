ede908729bbc7bc39c4ee8bd75e025d66444ee32
Dhruv Bhat <52482148+spaceman-dev@users.noreply.github.com>
2021-09-29 12:03:12 +0530

    Merge K sorted linked lists

---
+++ Heaps/MergeKSortedLinkedLists.cpp
@@ -0,0 +1,116 @@
+// { Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		printf("%d ", node->data);
+		node = node->next;
+	}
+	cout<<endl;
+}
+
+ // } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/ 
+
+class Solution{
+  public:
+    Node* merge(Node* node1, Node* node2)
+    {
+        if(!node1)
+            return node2;
+        if(!node2)
+            return node1;
+        if(node1->data < node2->data)
+        {
+            node1->next = merge(node1->next, node2);
+            return node1;
+        }
+        else
+        {
+            node2->next = merge(node1, node2->next);
+            return node2;
+        }
+    }
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K)
+    {
+        // Your code here
+        Node* node = NULL;
+        for(int i = 0 ; i < K ; i++)
+            node = merge(node, arr[i]);
+        return node;
+    }
+};
+
+
+
+// { Driver Code Starts.
+// Driver program to test above functions
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+	   int N;
+	   cin>>N;
+       struct Node *arr[N];
+       for(int j=0;j<N;j++)
+        {
+           int n;
+           cin>>n;
+
+           int x;
+           cin>>x;
+           arr[j]=new Node(x);
+           Node *curr = arr[j];
+           n--;
+
+           for(int i=0;i<n;i++)
+           {
+               cin>>x;
+               Node *temp = new Node(x);
+               curr->next =temp;
+               curr=temp;
+           }
+   		}
+   		Solution obj;
+   		Node *res = obj.mergeKLists(arr,N);
+		printList(res);
+
+   }
+
+	return 0;
+}
+  // } Driver Code Ends
--- Heaps/README.md
+++ Heaps/README.md
@@ -58,3 +58,17 @@
     extractMin() since the heap is empty thus
              no min element exist so -1
              is printed.
+
+## 5. Merge K Sorted Linked Lists:
+    Given K sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list.
+    Example 1:
+    Input:
+    K = 4
+    value = {{1,2,3},{4 5},{5 6},{7,8}}
+    Output: 1 2 3 4 5 5 6 7 8
+    Explanation: The test case has 4 sorted linked list of size 3, 2, 2, 2
+        1st    list     1 -> 2-> 3
+        2nd   list      4->5
+        3rd    list      5->6
+        4th    list      7->8
+        The merged list will be 1->2->3->4->5->5->6->7->8.
